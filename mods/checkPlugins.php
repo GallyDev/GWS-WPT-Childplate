926edeab9dd403da670506ee99c1215000c8a665
GallyDev <dev@gally-websolutions.com>
2023-05-15 16:52:07 +0200

    NEU:allPlugins.json and checkPlugins.php

---
+++ mods/checkPlugins.php
@@ -0,0 +1,48 @@
+<?php
+//  if ( ! function_exists( 'WP_Filesystem' ) ) {
+//     require_once ABSPATH . 'wp-admin/includes/file.php';
+// }
+
+class ckeckPlugins {
+   
+    private string $message = '';
+
+    public function getPluginsArray()
+    {
+       return json_decode(file_get_contents('allPlugins.json'));
+    }
+
+    public function wpSetMessage($type)
+    {
+       $this->message = '<div class="notice notice-'.$type.' is-dismissible">';
+       $this->message .= match($type) {
+        'info' => '<p></p>',
+        'warning' => '<p></p>',
+        'error' => '<p>Nicht alle Plugins sind installiert</p>',
+        'success' => '<p>Alle notwendingen Plugins sind installiert.</p>',
+       };
+       $this->message .='</div>';
+    }
+
+    public function wpSendMessage()
+    {
+        if ( current_user_can( 'administrator' ) ) {
+            echo $this->message;
+        }
+    }
+
+    public function isInstalled()
+    {
+        $pluginsObj = $this->getPluginsArray();
+        $neededPlugins = $pluginsObj->{"sp-plugins"}; //json_decode create objects from type stdClass not arrays
+        foreach($neededPlugins as $neededPlugin) {
+            if(is_plugin_active($neededPlugin->name)) {
+                $this->wpMessage('success');
+                add_action('admin_notices', [$this, 'wpSendMessage']);
+            }else{
+                $this->wpMessage('error');
+                add_action('admin_notices', [$this, 'wpSendMessage']);
+            }
+        }
+    }
+}
